0c70d6b84c9744d870d6d33e85370bc924e8fe13
Jonathan Dance <jd@wuputah.com>
2012-09-28 16:51:25 -0700

    change to l2met-style values

--- lib/kanshi/scrolls_reporter.rb
+++ lib/kanshi/scrolls_reporter.rb
@@ -3,8 +3,10 @@ require 'scrolls'
 class Kanshi::ScrollsReporter
 
   def report(name, url, data)
-    Scrolls.context(:name => name) do
-      Scrolls.log(data)
+    Scrolls.context(:name => name, :measure => true) do
+      data.each do |k, v|
+        Scrolls.log(:at => k, :last => v)
+      end
     end
   end
 
