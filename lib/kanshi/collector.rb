e25069cfcbc1926fe5fb9ee895eb2b098c921223
Pedro Belo <pedro@heroku.com>
2012-11-12 15:33:33 -0800

    support for postgres 9.2

--- lib/kanshi/collector.rb
+++ lib/kanshi/collector.rb
@@ -14,19 +14,29 @@ class Kanshi::Collector
 
   def with_db(&block)
     db = Sequel.connect(@url)
-    yield db
+    yield db, version(db)
   ensure
     db.disconnect if db
   end
 
+  def version(db)
+    raw = db["SELECT version()"].first[:version]
+    return unless version = raw.match(/\APostgreSQL (\d+\.\d+)/)
+    version[1].to_s
+  end
+
   def collect
     data = {}
-    with_db do |db|
-      ::Kanshi::Queries.each do |query|
+    with_db do |db, version|
+      queries(version).each do |query|
         data.merge! db[query, @db_name].first
       end
     end
     data
+  end
+
+  def queries(version)
+    Kanshi::Queries[version] || Kanshi::Queries[:default]
   end
 
 end
